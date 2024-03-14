acb9540a711a3dfd758ff05ed4ff44bc01cdfe4d
abdullah-124 <mdabdullahsakib124@gmail.com>
2024-03-14 23:57:39 +0600

    settings.py

--- django_portal/settings.py
+++ django_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +81,18 @@ WSGI_APPLICATION = 'django_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgres://django_portal_user:WogQjm9po96wh0oZRERAfeiiszkYDyYR@dpg-cnpjjl0l6cac73a8593g-a.oregon-postgres.render.com/django_portal',
+        # conn_max_age=600
+    )
 }
 
 
